32461a928d3fa9f38e694de3bc03357219669624
Ігор Костенко <kostenkoigor824@gmail.com>
2025-05-15 01:03:42 +0300

    Fix: added Flask for Render port check

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from database import init_db
 import config
 import logging
 import os
+from flask import Flask
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
@@ -20,9 +21,22 @@ init_db()
 dp.include_router(user.router)
 dp.include_router(admin.router)
 
+# Создаем Flask-приложение
+app = Flask(__name__)
+
+@app.route('/')
+def health_check():
+    return 'Bot is running', 200
+
 if __name__ == '__main__':
-    # Получаем PORT из переменных окружения (Render предоставляет его автоматически)
-    port = int(os.environ.get("PORT", 5000))
-    
-    # Запускаем бота с указанием порта
+    # Запускаем Flask в отдельном потоке
+    from threading import Thread
+    app_thread = Thread(target=app.run, kwargs={
+        'host': '0.0.0.0',
+        'port': int(os.environ.get("PORT", 5000)),
+    })
+    app_thread.daemon = True
+    app_thread.start()
+
+    # Запускаем бота
     dp.run_polling(bot, skip_updates=True, allowed_updates=dp.resolve_used_update_types())
